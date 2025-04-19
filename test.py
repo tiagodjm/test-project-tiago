a93b4af49a2b8952d50d4f7d20a201ce635f1daa
Tiago Machado <tiagodjm@gmail.com>
2025-04-20 01:01:13 +1000

    Test debug

---
+++ test.py
@@ -0,0 +1,17 @@
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+
+    print(total)
+
+calcular_preco_combo(30, 20)
+
+print('Programa finalizado')
+
+
+#F9 - Marcar-um-breakpoint
+#F5 - •Abrir-modo debug
+#• F10 - Passar sobre uma-linha, SEM entrar em método internos
+#- F11-•Passar sobre uma• linnha•e • ENTRAR • em método interno
